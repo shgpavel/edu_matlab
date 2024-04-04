004adce8ee0b2107fd3539322ccb3e70bcea5ca6
Pavel <shgpavel@yandex.ru>
2024-04-04 15:07:59 +0300

    upd

--- cont_1/control_1.m
+++ cont_1/control_1.m
@@ -14,18 +14,21 @@ i_ps = [];
 text(-3.5, 3.5, {'Выберите точки пересечения кривых',
  'кликая на графике.',
  'Для выхода из выбора нажмите Enter.',}, 'FontSize', 11);
-[x, y, button] = ginput();
 
-for i = 1:length(x)
-  if button(i) == 13
-    break;
-  endif
+a = zeros(100, 3);
 
-  i_p = fsolve(eq_comb, [x(i), y(i)]);
+for i = 1:100
+  [x, y, button] = ginput(1);
+  a(i, :) = [x, y, button];
+
+  i_p = fsolve(eq_comb, [a(i, 1), a(i, 2)]);
   i_ps = [i_ps; i_p];
   plot(i_p(1), i_p(2), '*', 'MarkerSize', 10, 'Color', 'r');
   text(i_p(1), i_p(2), sprintf('%.10f, %.10f', i_p(1), i_p(2)),
   'FontSize', 13, 'VerticalAlignment', 'bottom');
+  if a(i, 3) == 13
+    break
+  endif
 end
 %}
 
@@ -46,6 +49,7 @@ end
 
 %%  3
 
+
 eq_1 = @(theta) sqrt(2.*abs(cos(1.5.*theta)));
 eq_2 = @(x, y) x.^2 + 1.1.*x.*y - y.^2 + 5.*y - x - 6;
 a = [1, -2];
@@ -56,23 +60,40 @@ ezpolar(eq_1, [0, 2*pi]);
 grid on;
 ezplot(eq_2);
 plot(a(1), a(2), 'ro', 'MarkerSize', 5);
+title('');
 
-dist_y = @(x, y) ((((y-(5/2)).^2) / (1.1*x*y)) - a(2)).^2;
+dist_y = @(x, y) ((((y - (5/2)).^2) / (1.1*x*y)) - a(2)).^2;
 dist_x = @(x, y) ((((x - (1/2)).^2) / (1.1*x*y)) - a(1)).^2;
 dist_comb = @(xy) sqrt(dist_x(xy(1), xy(2)) + dist_y(xy(1), xy(2)));
 min_distance_point = fminunc(@(xy) dist_comb(xy), [1 1]);
-disp(min_distance_point);
 plot(min_distance_point(1), min_distance_point(2), 'ro', 'MarkerSize', 5);
 plot([a(1), min_distance_point(1)], [a(2), min_distance_point(2)], 'g--', 'LineWidth', 1);
+text_p = [(a(1) + min_distance_point(1)) / 2; (a(2) + min_distance_point(2)) / 2];
+min_dist = sqrt(min_distance_point(1).^2 + min_distance_point(2).^2);
+text(text_p(1), text_p(2), [num2str(min_dist ./ 2)], 'HorizontalAlignment', 'center');
+
 
 curve_points_x = @(theta) eq_1(theta) .* cos(theta);
 curve_points_y = @(theta) eq_1(theta) .* sin(theta);
 distances = @(theta) sqrt((curve_points_x(theta) - a(1)).^2 + (curve_points_y(theta) - a(2)).^2);
-min_dist = fminunc(@(theta) distances(theta), 0);
+min_dist = fminsearch(@(theta) distances(theta), 0);
 min_dist_p = [curve_points_x(min_dist), curve_points_y(min_dist)];
 
 plot(min_dist_p(1), min_dist_p(2), 'ro', 'MarkerSize', 5);
 plot([a(1), min_dist_p(1)], [a(2), min_dist_p(2)], 'g--', 'LineWidth', 1);
 text_p = [(a(1) + min_dist_p(1)) / 2; (a(2) + min_dist_p(2)) / 2];
-text(text_p(1), text_p(2), ['Мин. расстояние: ', num2str(min_distance)], 'HorizontalAlignment', 'center');
+min_dist = sqrt(min_dist_p(1).^2 + min_dist_p(2).^2);
+text(text_p(1), text_p(2), [num2str(min_dist)], 'HorizontalAlignment', 'center');
+
+dist_x = @(x, y, theta) ((((x - (1/2)).^2) / (1.1*x*y)) - curve_points_x(theta))^.2;
+dist_y = @(x, y, theta) ((((y - (5/2)).^2) / (1.1*x*y)) - curve_points_y(theta)).^2;
+dist_comb = @(w) sqrt(dist_x(w(1), w(2), w(3)) + dist_y(w(1), w(2), w(3)));
+min_distance_point = fminunc(@(w) dist_comb(w), [-1.4 1.18 (3/4)*pi]);
+plot(min_distance_point(1), min_distance_point(2), 'ro', 'MarkerSize', 5);
+min_dist_p = [curve_points_x(min_distance_point(3)), curve_points_y(min_distance_point(3))];
+plot(min_dist_p(1), min_dist_p(2), 'ro', 'MarkerSize', 5);
+plot([min_distance_point(1), min_dist_p(1)], [min_distance_point(2), min_dist_p(2)], 'g--', 'LineWidth', 1);
+text_p = [(min_distance_point(1) + min_dist_p(1)) / 2; (min_distance_point(2) + min_dist_p(2)) / 2];
+min_dist = sqrt((min_distance_point(1) - min_dist_p(1)).^2 + (min_distance_point(2) - min_dist_p(2)).^2);
+text(text_p(1), text_p(2), [num2str(min_dist)], 'HorizontalAlignment', 'center');
 
